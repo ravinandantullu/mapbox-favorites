da2b3bbea5a1b1db6a60a788e2e7e2be77462b66
ravinandantullu <ravinandantulluri@gmail.com>
2021-04-13 00:24:32 -0500

    Updates

---
+++ precache-manifest.48eea868534460a501cc7172c0f0cbab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac256cab7137797fcb7e",
+    "url": "/mapbox-favorites/static/js/main.ac256cab.chunk.js"
+  },
+  {
+    "revision": "8773db35b435222af0d4",
+    "url": "/mapbox-favorites/static/css/1.650dad83.chunk.css"
+  },
+  {
+    "revision": "8773db35b435222af0d4",
+    "url": "/mapbox-favorites/static/js/1.8773db35.chunk.js"
+  },
+  {
+    "revision": "f7db4aa00e1a2efb36a9",
+    "url": "/mapbox-favorites/static/js/runtime~main.f7db4aa0.js"
+  },
+  {
+    "revision": "8a70beeeea505c49824bc7197866cd15",
+    "url": "/mapbox-favorites/index.html"
+  }
+];
